fd19338b3ebda617d0cf295eb2703265f04cc7d3
Code Sandbox <hello@codesandbox.io>
2023-01-09 18:58:02 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.49f69b7b1278a88fbcc196efee01ce43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eda24074a446c77a8ad",
+    "url": "https://fakezin417.github.io/csb-77ey2g/static/css/main.59b71d22.chunk.css"
+  },
+  {
+    "revision": "9eda24074a446c77a8ad",
+    "url": "https://fakezin417.github.io/csb-77ey2g/static/js/main.9eda2407.chunk.js"
+  },
+  {
+    "revision": "d3158d9747f7637e38c9",
+    "url": "https://fakezin417.github.io/csb-77ey2g/static/js/1.d3158d97.chunk.js"
+  },
+  {
+    "revision": "7209f3c7ea6ecc5fce1b",
+    "url": "https://fakezin417.github.io/csb-77ey2g/static/js/runtime~main.7209f3c7.js"
+  },
+  {
+    "revision": "17ec1900f695a6646433ffdfe9d39cf5",
+    "url": "https://fakezin417.github.io/csb-77ey2g/index.html"
+  }
+];
